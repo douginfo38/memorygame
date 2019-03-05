66e19582d0aeb111b4cf3147861380e5e3620e55
Douglas Ramos <dougarainfo@gmail.com>
2019-03-05 00:28:59 -0300

    Initial code

---
+++ TrocarTela.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarTela : MonoBehaviour {
+
+	public void jogar(){
+		SceneManager.LoadScene ("cena1");
+	}
+
+	public void fim(){
+		SceneManager.LoadScene ("abertura");
+	}
+
+	public void Exit(){
+		Application.Quit();
+	}
+}
